17fb1fb5abccf9e175c2fee6f1872fce2e197e58
Candra <candra75245@gmail.com>
2025-08-30 06:29:00 +0700

    sec commit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route::get('/about', function () {
+//     return 'ini adlaha halaman about';
+// });
+Route::get('/users/{id}', function($id){
+    return "nilai id users adalah " .$id;
+});
+
+Route::get('/contact', function(){
+    return 'ini adalah halaman contact';
+})->name(name: 'contact');
+
+Route::get('/user/{name?}',function($name = "guest"){
+    return "hello, " .$name;
+});
+
+Route::prefix('manage')->group(function(){
+    Route::get('/edit', function() {
+        return 'ini adalah  halaman manage edit';
+    });
+    Route::get('/profile', function(){
+        return 'ini adalah halaman profile';
+    });
+    Route::get('/settings', function(){
+        return 'ini adalah halaman settings';
+    });
 });
