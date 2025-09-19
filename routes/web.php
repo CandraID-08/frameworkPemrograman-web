621e567237e0945b8cc17447e25f0aa9020c5204
Candra <candra75245@gmail.com>
2025-09-19 10:41:48 +0700

    praktikum 4

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -34,3 +35,10 @@ require __DIR__.'/auth.php';
 Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
+
+Route::get('/rahasigma', function () {
+    return 'this is a secret page';
+    // return view('dashboard');
+})->middleware(['auth', 'verified', 'RoleCheck:admin'])->name('rahasigma');
+
+Route::get('/produk',[ProductController::class, 'index']);
